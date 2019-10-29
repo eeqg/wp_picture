fb06807fa793efdb7ddba37a9d964a89a7489c94
eeqg <876583632@qq.com>
2019-10-29 11:45:38 +0800

    ++update.

---
+++ picture/src/main/java/com/wp/picture/widget/CommonViewPager.java
@@ -0,0 +1,306 @@
+package com.wp.picture.widget;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+
+
+import com.wp.picture.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by wp on 2019/5/9.
+ */
+public class CommonViewPager extends FrameLayout {
+    private int scaledTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+
+    private NoScrollViewPager viewPager;
+    private List<View> viewList = new ArrayList<>();
+    private List<Object> dataList = new ArrayList<>();
+
+    private ViewCreator viewCreator;
+    private OnItemClickListener itemClickListener;
+    private OnItemSelectedListener itemSelectedListener;
+    private OnSlideListener slideListener;
+    private float mLastX, mLastY;
+    private float widthOffset = 1f;
+    private View rootView;
+    protected boolean multiPage = false;
+    protected int itemPagerWidth;
+
+    public CommonViewPager(@NonNull Context context) {
+        this(context, null);
+    }
+
+    public CommonViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public CommonViewPager(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        // viewPager = new NoScrollViewPager(getContext());
+        // viewPager.setOffscreenPageLimit(5);
+        // viewPager.setClipChildren(false);
+        // setClipChildren(false);
+        // addView(viewPager, new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+        //         ViewGroup.LayoutParams.MATCH_PARENT));
+
+        rootView = LayoutInflater.from(getContext()).inflate(R.layout.layout_common_viewpager, this);
+        viewPager = rootView.findViewById(R.id.noScrollViewPager);
+        viewPager.setOffscreenPageLimit(5);
+    }
+
+    public ViewPager getViewPager() {
+        return this.viewPager;
+    }
+
+    public CommonViewPager setCanScroll(boolean canScroll) {
+        viewPager.setCanScroll(canScroll);
+        return this;
+    }
+
+    public void setCurrentItem(int position) {
+        viewPager.setCurrentItem(position);
+    }
+
+    public void setCurrentItem(int position, boolean smoothScroll) {
+        viewPager.setCurrentItem(position, smoothScroll);
+    }
+
+    public CommonViewPager setOnSlideListener(OnSlideListener listener) {
+        this.slideListener = listener;
+        return this;
+    }
+
+    public CommonViewPager addOnPageChangeListener(ViewPager.OnPageChangeListener listener) {
+        this.viewPager.addOnPageChangeListener(listener);
+        return this;
+    }
+
+    public CommonViewPager createItemView(ViewCreator creator) {
+        this.viewCreator = creator;
+        return this;
+    }
+
+    public CommonViewPager setOnItemClickListener(OnItemClickListener listener) {
+        this.itemClickListener = listener;
+        return this;
+    }
+
+    public CommonViewPager setOnItemSelectedListener(OnItemSelectedListener listener) {
+        this.itemSelectedListener = listener;
+        return this;
+    }
+
+    public CommonViewPager setMultiPage(boolean multiPage) {
+        this.multiPage = multiPage;
+        return this;
+    }
+
+    public CommonViewPager setItemPagerWidth(int itemWidth) {
+        this.itemPagerWidth = itemWidth;
+        return this;
+    }
+
+    public <T> void execute(List<T> dataList) {
+        if (dataList == null) {
+            return;
+        }
+        this.dataList.clear();
+        this.viewList.clear();
+        this.dataList.addAll(dataList);
+        generateItemViews();
+
+        viewPager.setAdapter(new Adapter());
+
+        if (itemSelectedListener != null) {
+            this.viewPager.addOnPageChangeListener(pageChangeListener);
+        }
+        if (multiPage) {
+            observeMultiPage();
+        }
+    }
+
+    private void generateItemViews() {
+        if (this.viewCreator == null) {
+            throw new RuntimeException("---请先创建itemView!!---");
+        }
+        for (int i = 0; i < dataList.size(); i++) {
+            View view = viewCreator.onCreateView(i);
+            this.viewList.add(view);
+            final int position = i;
+            this.viewCreator.onBindView(view, dataList.get(i), i);
+
+            if (itemClickListener != null) {
+                view.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        itemClickListener.onItemClick(v, dataList.get(position), position);
+                    }
+                });
+            }
+            if (itemSelectedListener != null && position == 0) {
+                itemSelectedListener.onItemSelected(dataList.get(0), 0);
+            }
+        }
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        float x = ev.getX();
+        float y = ev.getY();
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastX = x;
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dx = x - mLastX;
+                float dy = y - mLastY;
+                //向上滑动
+                if (dy < 0 && Math.abs(dy) > scaledTouchSlop && Math.abs(dy) > Math.abs(dx)) {
+                    if (slideListener != null) {
+                        slideListener.onUpSlide(getViewPager().getCurrentItem());
+                    }
+                }
+                //向左滑动
+                if (dx < 0 && Math.abs(dx) > scaledTouchSlop && Math.abs(dx) > Math.abs(dy)) {
+                    if (slideListener != null) {
+                        slideListener.onLeftSlide(getViewPager().getCurrentItem());
+                    }
+                }
+
+                mLastX = x;
+                mLastY = y;
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+
+        return super.dispatchTouchEvent(ev);
+    }
+
+    private void observeMultiPage() {
+        if (itemPagerWidth <= 0) {
+            itemPagerWidth = (getResources().getDisplayMetrics().widthPixels) / 3 + 20;
+        }
+        LinearLayout.LayoutParams layoutParams2 = (LinearLayout.LayoutParams) viewPager.getLayoutParams();
+        layoutParams2.width = itemPagerWidth;
+        viewPager.setLayoutParams(layoutParams2);
+
+        rootView.setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return viewPager.dispatchTouchEvent(event);
+            }
+        });
+
+        viewPager.setPageTransformer(true, new ViewPager.PageTransformer() {
+            private final float MAX_SCALE = 1.0f;
+            private final float MIN_SCALE = 0.9f;
+
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                if (position == 0) {
+                    page.setScaleX(MAX_SCALE);
+                    page.setScaleY(MAX_SCALE);
+                } else if (position < 0) {//滑出的页
+                    page.setScaleX(MAX_SCALE + (MAX_SCALE - MIN_SCALE) * position);
+                    page.setScaleY(MAX_SCALE + (MAX_SCALE - MIN_SCALE) * position);
+                } else if (position > 0) {//滑进的页
+                    page.setScaleX(MAX_SCALE - (MAX_SCALE - MIN_SCALE) * position);
+                    page.setScaleY(MAX_SCALE - (MAX_SCALE - MIN_SCALE) * position);
+                }
+            }
+        });
+    }
+
+    private class Adapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return viewList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == object;
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            View view = container.findViewWithTag(position);
+            if (view == null) {
+                view = viewList.get(position);
+                // view.setTag(position);
+                view.setTag(R.id.common_view_pager_container, position);
+                container.addView(view);
+            }
+            return view;
+        }
+
+        @Override
+        public void finishUpdate(@NonNull ViewGroup container) {
+            super.finishUpdate(container);
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((View) object);
+        }
+    }
+
+    private ViewPager.OnPageChangeListener pageChangeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            if (itemSelectedListener != null) {
+                itemSelectedListener.onItemSelected(dataList.get(position), position);
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+        }
+    };
+
+    public interface ViewCreator<V extends View, T> {
+        View onCreateView(int position);
+
+        void onBindView(V view, T data, int position);
+    }
+
+    public interface OnItemClickListener<V extends View, T extends Object> {
+        void onItemClick(V view, T data, int position);
+    }
+
+    public interface OnItemSelectedListener<T extends Object> {
+        void onItemSelected(T data, int position);
+    }
+
+    public interface OnSlideListener {
+        void onUpSlide(int position);
+
+        void onLeftSlide(int position);
+    }
+}
---
+++ picture/src/main/java/com/wp/picture/widget/TagLayoutView.java
@@ -0,0 +1,245 @@
+package com.wp.picture.widget;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.database.DataSetObserver;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import com.wp.picture.R;
+
+import java.util.List;
+
+/**
+ * Created by wp on 2019/5/5.
+ */
+public class TagLayoutView extends ViewGroup {
+	private final String TAG = "TagLayoutView";
+	
+	private TagViewAdapter tagAdapter;
+	private DataChangeObserve dataChangeObserve;
+	// private OnTagClickListener tagClickListener;
+	
+	private int tagSpace;
+	private int rowSpace;
+	
+	public TagLayoutView(Context context) {
+		this(context, null);
+	}
+	
+	public TagLayoutView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+	
+	public TagLayoutView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init(attrs);
+	}
+	
+	private void init(AttributeSet attrs) {
+		TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.TagLayoutView);
+		tagSpace = a.getDimensionPixelSize(R.styleable.TagLayoutView_tag_tag_space, (int) dp2px(8));
+		rowSpace = a.getDimensionPixelSize(R.styleable.TagLayoutView_tag_row_space, (int) dp2px(4));
+		a.recycle();
+	}
+	
+	private void addTagView() {
+		if (tagAdapter == null) {
+			return;
+		}
+		removeAllViews();
+		// LogUtils.d(TAG, "-----addTagView()--" + tagAdapter.getCount());
+		for (int index = 0; index < tagAdapter.getCount(); index++) {
+			View tagView = tagAdapter.getView(index, null, null);
+			addView(tagView);
+			// final int position = index;
+			// tagView.setOnClickListener(new OnClickListener() {
+			// 	@Override
+			// 	public void onClick(View v) {
+			// 		if (tagClickListener != null) {
+			// 			tagClickListener.onClicked(tagAdapter.getItem(position), position);
+			// 		}
+			// 	}
+			// });
+		}
+	}
+	
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int wantHeight = 0;
+		int wantWidth = resolveSize(0, widthMeasureSpec);
+		int paddingLeft = getPaddingLeft();
+		int paddingRight = getPaddingRight();
+		int paddingTop = getPaddingTop();
+		int paddingBottom = getPaddingBottom();
+		int childLeft = paddingLeft;
+		int childTop = paddingTop;
+		int lineHeight = 0;
+		
+		for (int i = 0; i < getChildCount(); i++) {
+			final View childView = getChildAt(i);
+			LayoutParams params = childView.getLayoutParams();
+			childView.measure(
+					getChildMeasureSpec(widthMeasureSpec, paddingLeft + paddingRight, params.width),
+					getChildMeasureSpec(heightMeasureSpec, paddingTop + paddingBottom, params.height)
+			);
+			int childHeight = childView.getMeasuredHeight();
+			int childWidth = childView.getMeasuredWidth();
+			lineHeight = Math.max(childHeight, lineHeight);
+			
+			if (childLeft + childWidth + paddingRight > wantWidth) {
+				childLeft = paddingLeft;
+				childTop += rowSpace + childHeight;
+				lineHeight = childHeight;
+			}
+			
+			childLeft += childWidth + tagSpace;
+		}
+		wantHeight += childTop + lineHeight + paddingBottom;
+		setMeasuredDimension(wantWidth, resolveSize(wantHeight, heightMeasureSpec));
+	}
+	
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		int width = r - l;
+		int paddingLeft = getPaddingLeft();
+		int paddingTop = getPaddingTop();
+		int paddingRight = getPaddingRight();
+		int childLeft = paddingLeft;
+		int childTop = paddingTop;
+		int lineHeight = 0;
+		
+		for (int i = 0; i < getChildCount(); i++) {
+			final View childView = getChildAt(i);
+			if (childView.getVisibility() == View.GONE) {
+				continue;
+			}
+			int childWidth = childView.getMeasuredWidth();
+			int childHeight = childView.getMeasuredHeight();
+			lineHeight = Math.max(childHeight, lineHeight);
+			
+			if (childLeft + childWidth + paddingRight > width) {
+				childLeft = paddingLeft;
+				childTop += rowSpace + lineHeight;
+				lineHeight = childHeight;
+			}
+			
+			childView.layout(childLeft, childTop, childLeft + childWidth, childTop + childHeight);
+			childLeft += childWidth + tagSpace;
+		}
+	}
+	
+	public void setAdapter(TagViewAdapter adapter) {
+		if (tagAdapter == null) {
+			this.tagAdapter = adapter;
+			if (dataChangeObserve == null) {
+				dataChangeObserve = new DataChangeObserve();
+				tagAdapter.registerDataSetObserver(dataChangeObserve);
+			}
+			addTagView();
+		}
+	}
+	
+	// public void setOnTagClickListener(OnTagClickListener listener) {
+	// 	this.tagClickListener = listener;
+	// }
+	
+	private class DataChangeObserve extends DataSetObserver {
+		@Override
+		public void onChanged() {
+			// LogUtils.d(TAG, "-----onChanged()--");
+			addTagView();
+		}
+	}
+	
+	//adapter
+	public static abstract class TagViewAdapter<T> extends BaseAdapter {
+		private List<T> tagList;
+		private OnTagClickListener tagClickListener;
+		
+		private int currentPosition = -1;
+		
+		public TagViewAdapter() {
+		}
+		
+		public void setTagList(List<T> list) {
+			// LogUtils.d("TagLayoutView", "-----setTagList()--" + list.size());
+			this.tagList = list;
+			notifyDataSetChanged();
+		}
+		
+		public void setCurrentPosition(int currentPosition) {
+			this.currentPosition = currentPosition;
+			notifyDataSetChanged();
+		}
+		
+		@Override
+		public int getCount() {
+			return tagList != null ? tagList.size() : 0;
+		}
+		
+		@Override
+		public T getItem(int position) {
+			return tagList.get(position);
+		}
+		
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+		
+		@Override
+		public View getView(final int position, View convertView, ViewGroup parent) {
+			ViewHolder holder;
+			if (convertView == null) {
+				convertView = createView();
+				holder = new ViewHolder(convertView);
+				convertView.setTag(holder);
+				
+				holder.itemView.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						if (tagClickListener != null) {
+							tagClickListener.onClicked(position);
+						}
+					}
+				});
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			bindView(holder, position);
+			// LogUtils.d("-----" + position + "-----" + currentPosition);
+			holder.itemView.setSelected(position == currentPosition);
+			return convertView;
+		}
+		
+		public class ViewHolder {
+			public View itemView;
+			
+			public ViewHolder(View view) {
+				itemView = view;
+			}
+		}
+		
+		protected abstract View createView();
+		
+		protected abstract void bindView(ViewHolder holder, int position);
+		
+		public void setOnTagClickListener(OnTagClickListener listener) {
+			this.tagClickListener = listener;
+		}
+		
+		public interface OnTagClickListener {
+			void onClicked(int position);
+		}
+	}
+	
+	private float dp2px(float dp) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp,
+				Resources.getSystem().getDisplayMetrics());
+	}
+}
